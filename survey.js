0760d95ed6f149c6be3eadbf6f8a4864bd5a579e
prnvthir14 <prnvthir@gmail.com>
2021-02-03 14:23:02 +0000

    added readline functionality to generate user profile

--- survey.js
+++ survey.js
@@ -1,9 +1,37 @@
-//this file will collect user input
+const readline = require('readline');
 
-//
-for (let u = 0; u < userInput.length; u++){
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  
+rl.question('What do you think of Node.js? ', (answer) => {
+
+  rl.question('Whats your name? ', (name) => {
+
+    rl.question('Favorite activity?', (activity) => {
+      
+      rl.question('Favorite meal?', (meal) => {
+
+        rl.question('Favorute food?', (food) => {
+
+          rl.question('Favorite activity post meal?', (postMealActivity) => {
+
+            rl.question('Favorite sport?', (sport) => {
+
+              let profile = `${name}s favorite activity is ${activity}. Their favorite meal is ${meal} and favorite food is ${food}. After food, 
+              ${name} likes to ${postMealActivity}. If on an empty stomach, ${name} loves to play ${sport}.`
+              
+              console.log(profile)
+              rl.close();
+
+            });
+
+          });
+        });
+      });
+    });   
+  });
+});
 
 
-}
